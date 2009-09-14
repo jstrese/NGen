5ce5472e9ce2811cf822e7f8bd8a9760980156dc
jstrese <jason.strese@gmail.com>
2009-09-14 19:54:57 +0000

    * Increased performance when setting custom include_path (no more string concatenation)

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 	error_reporting(E_ALL);
 	
-	ini_set('include_path', get_include_path() . PATH_SEPARATOR . './Core/' . PATH_SEPARATOR . './Core/Drivers/' . PATH_SEPARATOR . './Core/Interfaces/' . PATH_SEPARATOR . './Core/User Objects/' . PATH_SEPARATOR . './Core/Drivers/Page/' . PATH_SEPARATOR . './Core/Drivers/Database/');
+	if(PATH_SEPARATOR === ';')
+	{
+		ini_set('include_path', get_include_path() . ';./Core/;./Core/Drivers/;./Core/Interfaces/;./Core/User Objects/;./Core/Drivers/Page/;./Core/Drivers/Database/');
+	}
+	else
+	{
+		ini_set('include_path', get_include_path() . ':./Core/:./Core/Drivers/:./Core/Interfaces/:./Core/User Objects/:./Core/Drivers/Page/:./Core/Drivers/Database/');
+	}
 		
 	function __autoload($method)
 	{
-		include_once($method . '.php');
+		require_once($method . '.php');
 	}
-		
+
 	function exception_handler($exception)
 	{
 		try
@@ -19,14 +26,14 @@
 			echo $e->getMessage();
 		}
 	}
-	
+
 	set_exception_handler('exception_handler');
-	
+
 	function error_handler($errno, $errstr, $errfile, $errline)
 	{
 		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 	}
-	
+
 	require_once('./config.php');
 	
 	set_error_handler('error_handler');
@@ -36,6 +43,6 @@
 	$configs['__section'] = isset($_REQUEST['s']) ? $_REQUEST['s'] : '';
 	$configs['__action'] = isset($_REQUEST['a']) ? $_REQUEST['a'] : '';
 	NGenCore::$configs = $configs;
-	
+
 	Page::getInstance()->load();
 ?>
